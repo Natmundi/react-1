d94e17ae6145ff889ef96178ef3b3ceff747e73b
Natmundi <n.v.cherevata@outlook.com>
2023-10-19 22:45:24 +0200

    start airbnb

---
+++ src/component/price/index.js
@@ -0,0 +1,13 @@
+import "./index.css";
+import Box from "../box";
+
+export default function Price({
+  price,
+  discount,
+  currence,
+  cleaning,
+  checkin,
+  checkout,
+}) {
+  return <Box shadow>Text</Box>;
+}
